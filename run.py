69e6669d832c5207640f3c41d2a85ab3b63ca23f
akariasmorum <akariasmorum@gmail.com>
2019-03-08 21:39:56 +0500

    залил проект в контейнер

--- run.py
+++ run.py
@@ -1,10 +1,12 @@
 from config import app, db
 from auth.models import User
+from werkzeug.contrib.fixers import ProxyFix
 import api
 
 @app.shell_context_processor
 def make_shell_context():
 	return {'db': db, 'User': User}
 
+app.wsgi_app = ProxyFix(app.wsgi_app)
 if __name__ == '__main__':
 	app.run()
